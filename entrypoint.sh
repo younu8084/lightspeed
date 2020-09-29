1475668ce500cc562d0986a33f160c87ddaecb76
younu8084 <younu8084@gmail.com>
2020-09-29 17:01:10 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@ sudo yum install docker
 sudo service docker start
 sudo curl -L "https://github.com/docker/compose/releases/version/dowload/1.27.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-docker volume create postgres-volume
-docker-compose up
+sudo docker volume create postgres-volume
+sudo docker-compose up
